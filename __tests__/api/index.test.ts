69810aec66bfce8e7fe40d91be79ae4432bc1d95
VinGuler <vinguler@gmail.com>
2024-12-22 22:24:12 +0200

    test: added fetch mocker

--- __tests__/api/index.test.ts
+++ __tests__/api/index.test.ts
@@ -1,7 +1,24 @@
-import { expect, test } from 'vitest';
+import { beforeAll, expect, test, vi } from 'vitest';
+import createFetchMock, { type FetchMock } from 'vitest-fetch-mock';
 import { getCountryByIp } from '@/api';
 
-test('request api', async () => {
+let fetchMocker: FetchMock;
+
+beforeAll(() => {
+  fetchMocker = createFetchMock(vi);
+  fetchMocker.enableMocks();
+});
+
+test('request api should return empty properties', async () => {
+  fetchMocker.doMock('{}');
   const result = await getCountryByIp('');
-  expect(result).toBeTruthy();
+  expect(result.countryCode).toBeFalsy();
+  expect(result.timezone).toBeFalsy();
+});
+
+test('request api should return properties', async () => {
+  fetchMocker.doMock('{ "country": "IL", "timezone": "Jerusalem/Israel" }');
+  const result = await getCountryByIp('');
+  expect(result.countryCode).toBe('IL');
+  expect(result.timezone).toBe('Jerusalem/Israel');
 });
